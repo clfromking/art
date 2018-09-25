277e30b49d04ad0b711c9e48975e769ce75082f0
QSNKZXYXME6PTPK\Administrator <949246160@qq.com>
2018-09-25 17:57:20 +0800

    查看所有助力人和参与人添加分页

--- wechat_artally_send/pages/joinpeople/joinpeople.js
+++ wechat_artally_send/pages/joinpeople/joinpeople.js
@@ -1,5 +1,6 @@
 // pages/joinpeople/joinpeople.js
 const app=getApp()
+var title = '参与'
 Page({
 
   /**
@@ -8,7 +9,13 @@ Page({
   data: {
     heads: [
     ],
-    title:''
+    title:'',
+    pages:1,
+    total:0,
+    inviter:0,
+    order_id:0,
+    uid:0,
+    iscontinue:true
   },
 
   //预览大图
@@ -24,26 +31,36 @@ Page({
   onLoad: function (options) {
     console.log(options)
     var that=this
-    var title='参与'
     var postData={}
+    var order_id=options.order_id
+    var uid=options.uid
+    var inviter = options.inviter ? options.inviter:''
+    that.setData({
+      order_id,
+      uid,
+      inviter
+    })
     if (options.inviter) {
-      postData = { 'order_id': options.order_id, 'uid': options.uid,'inviter':options.inviter}
+      postData = { 'order_id': options.order_id, 'uid': options.uid,'inviter':options.inviter,'pages':that.data.pages}
       title='为你助力'
     }
     else{
-      postData = { 'order_id': options.order_id, 'uid': options.uid}
+      postData = { 'order_id': options.order_id, 'uid': options.uid, 'pages': that.data.pages}
+      title='参加'
     }
-    app.post('order/client_list',postData).then((res)=>{
+    app.post('order/client_list', postData).then((res) => {
       console.log(res)
-      if(res.code==200){
+      if (res.code == 200) {
         that.setData({
-          heads:res.data.list,
-          title
+          heads: res.data.list,
+          title,
+          total: res.data.total
         })
       }
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error)
     })
+    // that.loadavatar(postData,title)
   },
 
   /**
@@ -85,7 +102,41 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    if (this.data.iscontinue==false){
+      return
+    }
+    var pages=this.data.pages
+    pages=pages+1
+    console.log(pages)
+    this.setData({
+      pages:pages
+    })
+    var postData={}
+    var iscontinue=true
+    if (this.data.title=='为你助力') {
+      postData = { 'order_id': this.data.order_id, 'uid': this.data.uid, 'inviter': this.data.inviter, 'pages': this.data.pages }
+      title = '为你助力'
+    }
+    else {
+      postData = { 'order_id': this.data.order_id, 'uid': this.data.uid, 'pages': this.data.pages }
+      title = '参加'
+    }
+    var that=this
+    app.post('order/client_list',postData).then((res)=>{
+      var imgData = that.data.heads
+      for(var i=0;i<res.data.list.length;i++){
+        imgData.push(res.data.list[i])
+      }
+      if(res.data.list.length<=0){
+        iscontinue=false
+      }    
+      that.setData({
+        heads: imgData,
+        iscontinue
+      })
+    }).catch((error)=>{
+      console.log(error)
+    })
   },
 
   /**
